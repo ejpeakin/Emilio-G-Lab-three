12f86ee122af8371693cd63056b9821c99d9aabb
ejpeakin <egarcia29@setoncatholic.org>
2026-09-23 20:18:55 +0000

    change

---
+++ main.py
@@ -0,0 +1,30 @@
+######################## addition function ################################
+def add(x,y):
+    print(x + y)
+
+x = int(input("enter first number :"))
+y = int(input("enter second number :")) 
+####################### subtract function ############################# 
+def subtract(x,y):
+    print(x-y)
+
+x = int(input("enter first number :"))
+y = int(input("enter second number :"))
+###################### multiply function ################################
+def multiply(x,y):
+    print(x*y)
+
+x = int(input("enter first number :"))
+y = int(input("enter second number :"))
+###################### divisiion function ###################################
+def divide(x,y):
+    print(x/y)
+
+x = int(input("enter first number :"))
+y = int(input("enter second number :"))
+
+##############################################################################
+add(x,y)
+subtract(x,y)
+multiply(x,y)
+divide(x,y)
