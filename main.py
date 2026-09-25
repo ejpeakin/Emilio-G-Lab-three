5a557e0eb76213a489b1b0363293d5c0376a4975
ejpeakin <egarcia29@setoncatholic.org>
2026-09-25 15:42:41 +0000

    calc code

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
-######################## addition function ################################
-def add(x,y):
-    print(x + y)
 
-x = int(input("enter first number :"))
-y = int(input("enter second number :")) 
-####################### subtract function ############################# 
-def subtract(x,y):
-    print(x-y)
-
-x = int(input("enter first number :"))
-y = int(input("enter second number :"))
-###################### multiply function ################################
-def multiply(x,y):
-    print(x*y)
-
-x = int(input("enter first number :"))
-y = int(input("enter second number :"))
-###################### divisiion function ###################################
-def divide(x,y):
-    print(x/y)
-
-x = int(input("enter first number :"))
-y = int(input("enter second number :"))
-
-##############################################################################
-add(6,7)
-subtract(6,7)
-multiply(6,7)
-divide(6,7)
 ####################### CALCULATOR CODE ##################################
 print("whats good homeboy")
 print("what we doing")
-print("type (a)dd s(subtract) (m)ultiply (d)ivide (q)uit)")
+def add (x,y):
+    print(x+y)
+def multiply (x,y):
+    print (x*y)
+def divide (x,y):
+    print(x/y)
+def subtract (x,y):
+    print(x-y)
+while(True):
+    print("type (a)dd s(subtract) (m)ultiply (d)ivide (q)uit)")
+    user_choice = input(":")
+#add
+    if user_choice == "a":
+        x=int(input("first #"))
+        y=int(input("second #"))
+        add(x,y) 
+
+#subtract
+    elif user_choice == "s":
+        x=int(input("first #"))
+        y=int(input("second #"))
+        subtract(x,y)
+
+#multiply
+    elif user_choice == "m":
+        x=int(input("first #"))
+        y=int(input("second #"))
+        multiply(x,y)
+
+#divide
+    elif user_choice == "d":
+        x=int(input("first #"))
+        y=int(input("second #"))
+        divide(x,y)
+
+    elif user_choice == 'q':
+        break
+    else:
+        print("stupid you cant do that")
+
+
 
